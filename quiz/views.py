da93477121a228070ba4e43b943fa5775fd724c5
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2024-12-07 18:52:37 +0330

    feat(quiz): Add custom permissions for updating and deleting quizzes
    - Update QuizViewSet to include custom permissions for update, partial_update, and destroy actions
    - Implement IsOwnerOrAdmin permission class to restrict modifications to quiz authors and superusers
    - Ensure list, create, and retrieve actions are accessible to authenticated users
    - Override get_permissions method to apply appropriate permissions based on action

--- quiz/views.py
+++ quiz/views.py
@@ -3,12 +3,20 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from .models import Quiz, Question
 from .serializers import QuizSerializer
+from .permissions import IsOwnerOrAdmin
 
 
 class QuizViewSet(ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self, *args, **kwargs):
+        if self.action in ['list', 'create', 'retrieve']:
+            return [IsAuthenticated()]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            return [IsOwnerOrAdmin()]
+        
+        return super().get_permissions()
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
