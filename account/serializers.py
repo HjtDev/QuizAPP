69785b4d6cf9c2737c1bd447dd8a485f087d1ec1
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2024-12-07 17:46:59 +0330

    feat(auth): Implement user registration API
    - Create RegisterView with CreateAPIView for user registration
    - Define RegisterSerializer for handling user registration data
    - Validate user inputs such as phone, name, display_name, and password
    - Add validation for phone number format and uniqueness
    - Create new Player instance with validated data
    - Configure API URL for user registration endpoint

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import Serializer, ModelSerializer, CharField, ValidationError
 from .models import Player
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, password_validation
 
 
 class PlayerSerializer(ModelSerializer):
@@ -21,3 +21,39 @@ class LoginSerializer(Serializer):
             raise ValidationError('Invalid credentials')
         attrs['user'] = user
         return attrs
+
+
+class RegisterSerializer(Serializer):
+    phone = CharField(required=True)
+    name = CharField(required=True)
+    display_name = CharField(required=True)
+    password = CharField(required=True, write_only=True)
+
+    def validate(self, attrs):
+        phone = attrs.get('phone')
+        name: str = attrs.get('name')
+        display_name = attrs.get('display_name')
+        password = attrs.get('password')
+
+        if not (phone and phone.startswith('09') and len(phone) == 11 and phone.isdigit()):
+            raise ValidationError('Invalid phone number')
+
+        if Player.objects.filter(phone=phone).exists():
+            raise ValidationError('Phone number already in use')
+
+        if not (name and name.isascii()):
+            raise ValidationError('Invalid name')
+
+        if not display_name or len(display_name) < 3:
+            raise ValidationError('Invalid display_name')
+
+        password_validation.validate_password(password)
+
+        return attrs
+
+    def create(self, validated_data):
+        player = Player(phone=validated_data['phone'], name=validated_data['name'],
+                        display_name=validated_data['display_name'])
+        player.set_password(validated_data['password'])
+        player.save()
+        return player
--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Player
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import LoginSerializer, PlayerSerializer
+from rest_framework.generics import CreateAPIView
+from .serializers import LoginSerializer, PlayerSerializer, RegisterSerializer
 
 
 class LoginView(APIView):
@@ -15,3 +17,8 @@ class LoginView(APIView):
             return Response(player_data)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+    queryset = Player.objects.all()
