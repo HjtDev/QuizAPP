c2bc1db84d06166abee4276dd8d4dbba25a9d26b
Mohammad Hojjat Nikoobakht <m.h.nikoobakht@gmail.com>
2024-12-15 13:10:45 +0330

    chore: Migrate database from SQLite3 to PostgreSQL
    - Update database settings in settings.py to configure PostgreSQL

--- QuizAPP/settings.py
+++ QuizAPP/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'QuizAPP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quiz_app',
+        'USER': 'quiz_app_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
